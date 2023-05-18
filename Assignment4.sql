5f6e08164746f368b5a091dbd3a05dba0cb3c527
Sanskriti <104347305+Sanskritis101@users.noreply.github.com>
2023-05-18 22:55:33 +0530

    Update Assignment4.sql

--- Assignment4.sql
+++ Assignment4.sql
@@ -4,13 +4,22 @@ Query OK, 1 row affected (0.56 sec)
 mysql> use online_bookstore;
 Database changed
 
-create table author(
-    -> a_id int primary key,
-    -> a_name varchar(255) unique,
+mysql> create table author(
+    -> a_name varchar(255) primary key,
     -> phone int,
     -> email_id varchar(100)
     -> );
 Query OK, 0 rows affected (0.26 sec)
+
+mysql> desc author;
++----------+--------------+------+-----+---------+-------+
+| Field    | Type         | Null | Key | Default | Extra |
++----------+--------------+------+-----+---------+-------+
+| a_name   | varchar(255) | NO   | PRI | NULL    |       |
+| phone    | int          | YES  |     | NULL    |       |
+| email_id | varchar(100) | YES  |     | NULL    |       |
++----------+--------------+------+-----+---------+-------+
+3 rows in set (0.45 sec)
 
 mysql> create table publisher(
     -> P_name varchar(255) primary key,
